4fda7232d6b36a3efc0b46355b04301d0e08fcc7
rafaelzf <polako.rafa@gmail.com>
2024-09-03 00:14:01 -0300

    remove old files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import ResearchCards from "./ResearchCards";
-import SearchAllFiles from "./SearchAllFiles";
-import FileUploader from "./FileUploader";
 import ComboBox from "./ComboBox";
-import Datatable from "./Datatable";
-import CreateCategories from "./CreateCategories";
-import EditCategories from "./EditCategories";
 
 import Fields from "./Fields/";
 import GoToPapers from "./GoToPapers";
@@ -26,13 +20,7 @@ import LinkCategories from "./LinkCategories";
 export {
   Header,
   Footer,
-  ResearchCards,
-  SearchAllFiles,
-  FileUploader,
   ComboBox,
-  Datatable,
-  CreateCategories,
-  EditCategories,
   Fields,
   GoToPapers,
   PapersList,
